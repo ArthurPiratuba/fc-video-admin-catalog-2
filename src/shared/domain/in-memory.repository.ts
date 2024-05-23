afeadee88aa24067b43f2922c5a7abd5580ec096
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-05-22 21:30:16 -0300

    Added in memory repositpry tests

--- src/shared/domain/in-memory.repository.ts
+++ src/shared/domain/in-memory.repository.ts
@@ -1,10 +1,11 @@
 import { Entity } from "./entity";
+import { NotFoundError } from "./errors/not-found.error";
 import { Repository } from "./repository.interface";
 import { ValueObject } from "./value-object";
 
 export abstract class InMemoryRepository<T extends Entity, EntityId extends ValueObject> implements Repository<T, EntityId> {
 
-    protected items: T[] = [];
+    items: T[] = [];
 
     async insert(entity: T): Promise<void> {
         this.items.push(entity);
@@ -16,23 +17,23 @@ export abstract class InMemoryRepository<T extends Entity, EntityId extends Valu
 
     async update(entity: T): Promise<void> {
         const indexFound = this.items.findIndex(item => item.entity_id.equals(entity.entity_id));
-        if (indexFound === -1) throw new Error("Entity not found");
+        if (indexFound === -1) throw new NotFoundError(entity.entity_id, this.getEntity());
         this.items[indexFound] = entity;
     }
 
-    async delete(id: EntityId): Promise<void> {
-        const indexFound = this.items.findIndex(item => item.entity_id.equals(id));
-        if (indexFound === -1) throw new Error("Entity not found");
+    async delete(entity_id: EntityId): Promise<void> {
+        const indexFound = this.items.findIndex(item => item.entity_id.equals(entity_id));
+        if (indexFound === -1) throw new NotFoundError(entity_id, this.getEntity());
         this.items.splice(indexFound, 1);
     }
 
-    async findById(id: EntityId): Promise<T | null> {
-        const item = this.items.find(item => item.entity_id.equals(id));
+    async findById(entity_id: EntityId): Promise<T | null> {
+        const item = this.items.find(item => item.entity_id.equals(entity_id));
         return typeof item === "undefined" ? null : item;
     }
 
     async findAll(): Promise<any[]> {
-        throw new Error("Method not implemented.");
+        return this.items;
     }
 
     abstract getEntity(): new (...args: any[]) => any;
