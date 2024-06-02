04bbfe1e2d6bb2659e2d69bfe6957a1a963ebe16
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-06-02 15:58:56 -0300

    Added category-in-memory-repository tests

--- src/category/infra/category-in-memory.repository.ts
+++ src/category/infra/category-in-memory.repository.ts
@@ -1,10 +1,23 @@
-import { InMemoryRepository } from "../../shared/infra/in-memory.repository";
+import { SortDirection } from "../../shared/domain/search-params";
+import { InMemorySearchableRepository } from "../../shared/infra/in-memory.repository";
 import { Category } from "../domain/category.entity";
 import { Uuid } from "../domain/uuid.vo";
 
-export class CategoryRepositoryMemory extends InMemoryRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+
+    sortableFields: string[] = ['name', 'create_at'];
+
+
+    protected async applyFilter(items: Category[], filter: string): Promise<Category[]> {
+        if (!filter) return items;
+        return items.filter(i => i.name.toLowerCase().includes(filter.toLowerCase()));
+    }
 
     getEntity(): new (...args: any[]) => Category {
         return Category;
+    }
+
+    protected applySort(items: Category[], sort: string | null, sort_dir: SortDirection | null) {
+        return sort ? super.applySort(items, sort, sort_dir) : super.applySort(items, 'created_at', 'desc');
     }
 }
